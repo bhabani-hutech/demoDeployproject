c117944d82f04cb8562d6236dab718b281f4e2a3
bhabani222 <bhabani.hutech@gmail.com>
2021-11-08 13:51:04 +0530

    my new changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard/Dashboard";
 // import Details from "./Pages/Details/Details";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component= {Dashboard}/>
-        {/* <Route exact path="/details" component= {Details}/> */}
-      </Switch>
-    </div>
+      <div className="App">
+        <Routes>
+          {/* <Route path="/welcome" element={<Home />} /> */}
+          <Route exact path="/" element={<Dashboard />} />
+          {/* <Route exact path="/details" component= {Details}/> */}
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
